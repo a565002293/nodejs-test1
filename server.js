231087f1163c3bdb7d409a42426a50122e7d1bb4
LDQ <565002293@qq.com>
2020-01-20 22:42:12 +0800

    ddddd

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`欢迎来到我的网页`)
+        response.write(`  
+            <link rel="stylesheet" href="/x">
+            <body><h1>欢迎来到我的网页</h1></body>
+            `
+        );
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
