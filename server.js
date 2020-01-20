d4b50a1040fc68b85fc2a94b93a16bdf0530c43e
LDQ <565002293@qq.com>
2020-01-20 22:24:02 +0800

    最终

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    console.log('请输入\nnode server.js 8888')
     process.exit(1)
 }
 
@@ -17,14 +17,13 @@ var server = http.createServer(function (request, response) {
     var query = parsedUrl.query
     var method = request.method
 
-    /******** 从这里开始看，上面不要看 ************/
 
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    console.log('收到请求，路径（带查询参数）为：' + pathWithQuery)
 
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        response.write(`欢迎来到我的网页`)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
@@ -38,8 +37,7 @@ var server = http.createServer(function (request, response) {
         response.end()
     }
 
-    /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n http://localhost:' + port)
